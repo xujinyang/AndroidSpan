8daf3755e35873cc1445a5f77efb9cdb78a96d91
xujinyang <1119117546@qq.com>
2015-10-17 12:39:50 +0800

    上传到jcenter

--- app/src/main/java/jamesxu/androidspan/MainActivity.java
+++ app/src/main/java/jamesxu/androidspan/MainActivity.java
@@ -1,10 +1,9 @@
 package jamesxu.androidspan;
 
+import android.graphics.BlurMaskFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.SpannableString;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.TextView;
 
 import me.jamesxu.androidspan.AndroidSpan;
@@ -14,8 +13,6 @@ import me.jamesxu.androidspan.SpanOptions;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private AccelerateDecelerateInterpolator mSmoothInterpolator;
-    private SpannableString spannableString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
                 .drawUnderlineSpan("测试文本")
                 .drawRelativeSize("我要变大", 2.0f)
                 .drawForegroundColor("红色的文本", Color.RED)
-                .drawWithOptions("综合效果", new SpanOptions().addBackgroundColorSpan(Color.GREEN).addUnderlineSpan());
+                .drawWithOptions("综合效果", new SpanOptions().addBackgroundColorSpan(Color.GREEN).addUnderlineSpan())
+                .drawImageSpan("来个图片试试", getApplicationContext(), R.mipmap.ic_launcher)
+                .drawBulletSpan("bullet", 20, Color.GREEN)
+                .drawStrikethroughSpan("中线穿过")
+                .drawURLSpan("www.baidu.com")
+                .drawMaskFilterSpan("模糊效果", 1f, BlurMaskFilter.Blur.NORMAL);
+
         textView.setText(androidSpan.getSpanText());
 
     }
